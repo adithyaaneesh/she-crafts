2018cb5c144429a86afd74e056e5f21174823b5d
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-08 16:48:37 +0530

    Add shopitems

---
+++ src/data/Shop.js
@@ -0,0 +1,20 @@
+export const Product = [
+    {
+        img:'/images/crochetpattern.jpg',
+        name:"Crochet pattern",
+        description:"Handmade crochet pattern",
+        price:'120'
+    },
+    {
+        img:'/images/crochettop.jpg',
+        name:"Crochet Top",
+        description:"Handmade crochet top",
+        price:'320'
+    },
+    {
+        img:'/images/crochetfrog.jpg',
+        name:"Crochet Frog",
+        description:"Handmade crochet pafrogttern",
+        price:'420'
+    },
+]
